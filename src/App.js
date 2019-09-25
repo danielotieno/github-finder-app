b6250c62b9f2b5c50ccde2e42fa3246a6072d8f1
Daniel Otieno <danielotieno.ke@gmail.com>
2019-09-25 16:14:46 +0300

    chore(Nav): create a navbar with title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello Danny Boy</h1>
+        <Navbar />
       </div>
     );
   }
