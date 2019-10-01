f9f2b08d293bf89cca47a23ef095093f1f8a6b76
Daniel Otieno <danielotieno.ke@gmail.com>
2019-10-01 12:39:03 +0300

    chore:add spinner component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,29 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
 import Axios from "axios";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getUsers();
   }, []);
 
   const getUsers = async () => {
+    setLoading(true);
+
     const githubUsers = await Axios.get("https://api.github.com/users");
     const { data } = githubUsers;
-    console.log(data);
 
     setUsers(data);
+    setLoading(false);
   };
 
-  return (
-    <div className="grid-3">
-      {users.map(user => (
-        <UserItem key={user.id} user={user} />
-      ))}
-    </div>
-  );
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className="grid-3">
+        {users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Users;
